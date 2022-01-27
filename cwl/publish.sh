c0d6a06aa663da667856eeba421f4e5742cfe790
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-01-27 10:13:13 -0800

    fix paths in publish script

--- cwl/publish.sh
+++ cwl/publish.sh
@@ -11,10 +11,10 @@ sbpack bdc smgogarten/genesis-relatedness/ld-pruning-pipeline ld-pruning-wf.cwl
 python3 test/test_ld_pruning.py
 
 # push to pre-build project
-sbpack bdc smgogarten/genesis-relatedness-pre-build/ld-pruning ld-pruning-wf.cwl.steps/ld_pruning.cwl
-sbpack bdc smgogarten/genesis-relatedness-pre-build/subset-gds ld-pruning-wf.cwl.steps/subset_gds.cwl
-sbpack bdc smgogarten/genesis-relatedness-pre-build/merge-gds ld-pruning-wf.cwl.steps/merge_gds.cwl
-sbpack bdc smgogarten/genesis-relatedness-pre-build/check-merged-gds ld-pruning-wf.cwl.steps/check_merged_gds.cwl
+sbpack bdc smgogarten/genesis-relatedness-pre-build/ld-pruning tools/ld_pruning.cwl
+sbpack bdc smgogarten/genesis-relatedness-pre-build/subset-gds tools/subset_gds.cwl
+sbpack bdc smgogarten/genesis-relatedness-pre-build/merge-gds tools/merge_gds.cwl
+sbpack bdc smgogarten/genesis-relatedness-pre-build/check-merged-gds tools/check_merged_gds.cwl
 
 # pull tools with new app ids from pre-build
 sbpull bdc smgogarten/genesis-relatedness-pre-build/ld-pruning tools/ld_pruning.cwl
