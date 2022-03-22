6657c501eecaecffdf62bf2d549b5a03d2148e28
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-03-22 10:23:01 -0700

    build GENESIS from github in workflow docker

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,2 +1,2 @@
 docker build -t uwgac/genesis:2.24.0 -f genesis.dfile .
-docker build -t uwgac/genesis-workflow:3.0.0 -f genesis-workflow.dfile .
+docker build --no-cache -t uwgac/genesis-workflow:3.0.0 -f genesis-workflow.dfile .
