7a96d135aef3ecf3200257c49c6c886a4e1f6ab9
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-03-27 20:38:56 -0700

    update docker image to GENESIS 2.24.2

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,2 +1,2 @@
-docker build -t uwgac/genesis:2.24.0 -f genesis.dfile .
+docker build --no-cache -t uwgac/genesis:2.24.2 -f genesis.dfile .
 docker build --no-cache -t uwgac/genesis-workflow:3.0.0 -f genesis-workflow.dfile .
