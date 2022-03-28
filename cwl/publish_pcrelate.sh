13da95237235e16ab3cfe60b00d85e5a03ad7730
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-03-27 21:27:32 -0700

    typo

--- cwl/publish_pcrelate.sh
+++ cwl/publish_pcrelate.sh
@@ -5,6 +5,7 @@ sbpack bdc smgogarten/genesis-relatedness/pcrelate-beta tools/pcrelate_beta.cwl
 sbpack bdc smgogarten/genesis-relatedness/pcrelate tools/pcrelate.cwl
 sbpack bdc smgogarten/genesis-relatedness/pcrelate-correct tools/pcrelate_correct.cwl
 sbpack bdc smgogarten/genesis-relatedness/sample-blocks tools/sample_blocks.cwl
+sbpack bdc smgogarten/genesis-relatedness/kinship-plots tools/kinship_plots.cwl
 sbpack bdc smgogarten/genesis-relatedness/pc-relate pc-relate-wf.cwl
 
 # test
@@ -15,13 +16,15 @@ python3 test/test_pcrelate_noplots.py
 sbpack bdc smgogarten/genesis-relatedness-pre-build/pcrelate-beta tools/pcrelate_beta.cwl
 sbpack bdc smgogarten/genesis-relatedness-pre-build/pcrelate tools/pcrelate.cwl
 sbpack bdc smgogarten/genesis-relatedness-pre-build/pcrelate-correct tools/pcrelate_correct.cwl
-sbpack bdc smgogarten/genesis-relatedness-pre-build/sample-blocks tools/sample_blocks.cw
+sbpack bdc smgogarten/genesis-relatedness-pre-build/sample-blocks tools/sample_blocks.cwl
+sbpack bdc smgogarten/genesis-relatedness-pre-build/kinship-plots tools/kinship_plots.cwl
 
 # pull tools with new app ids from pre-build
 sbpull bdc smgogarten/genesis-relatedness-pre-build/pcrelate-beta tools/pcrelate_beta.cwl
 sbpull bdc smgogarten/genesis-relatedness-pre-build/pcrelate tools/pcrelate.cwl
 sbpull bdc smgogarten/genesis-relatedness-pre-build/pcrelate-correct tools/pcrelate_correct.cwl
 sbpull bdc smgogarten/genesis-relatedness-pre-build/sample-blocks tools/sample_blocks.cwl
+sbpull bdc smgogarten/genesis-relatedness-pre-build/kinship-plots tools/kinship_plots.cwl
 
 # push workflow to pre-build
 sbpack bdc smgogarten/genesis-relatedness-pre-build/pc-relate pc-relate-wf.cwl
